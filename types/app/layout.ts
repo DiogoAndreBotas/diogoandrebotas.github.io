33de06467117c4d3ca5a074d2e28ae849c15d2ad
DiogoAndreBotas <gamebreak24@gmail.com>
2024-12-14 17:23:11 +0000

    Updates

--- types/app/layout.ts
+++ types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Dev\diogoandrebotas.github.io\personal-website\src\app\layout.tsx
+// File: C:\Dev\diogoandrebotas.github.io\personal-project\src\app\layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
